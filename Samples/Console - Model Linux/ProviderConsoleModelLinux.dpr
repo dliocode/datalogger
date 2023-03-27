2e22ecbb391df5dd4a31a009056e28a7330c9438
Danilo Lucas <developer.dlio@gmail.com>
2023-03-27 11:23:36 -0300

    New Sample

---
+++ Samples/Console - Model Linux/ProviderConsoleModelLinux.dpr
@@ -0,0 +1,62 @@
+program ProviderConsoleModelLinux;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  DataLogger,
+  DataLogger.Provider.Console,
+  System.SysUtils;
+
+begin
+  // Add Provider
+  Logger.AddProvider(TProviderConsole.Create);
+
+  // Change the level name
+  Logger.SetLevelName(TLoggerLevel.Success, '    OK    ');
+  Logger.SetLevelName(TLoggerLevel.Error,   '   FAIL   ');
+  Logger.SetLevelName(TLoggerLevel.Fatal,   ' CRITICAL ');
+  Logger.SetLevelName(TLoggerLevel.Warn,    ' LOADING  ');
+  Logger.SetLevelName(TLoggerLevel.Info,    '   INFO   ');
+
+  // Defining the format
+  Logger.SetLogFormat(Format(' [%s] %s: %s', [TLoggerFormat.LOG_LEVEL, TLoggerFormat.LOG_TIMESTAMP, TLoggerFormat.LOG_MESSAGE]));
+
+  // Generating the logs
+  Logger.Info('Test with the function: SetLevelName');
+  Sleep(2000);
+  Logger.SlineBreak;
+  Logger.Success('Database connection');
+  Sleep(2000);
+  Logger.Error('save records');
+  Sleep(2000);
+  Logger.Fatal('System failure');
+  Sleep(2000);
+  Logger.SlineBreak;
+  Logger.SlineBreak;
+
+  // System loading simulation - using UndoLastLine
+  Logger.Info('Test with the function: UndoLastLine');
+  Sleep(2000);
+
+  Logger.SlineBreak;
+  Logger.Warn('Load System - Kernel');
+  Sleep(2000);
+  Logger.UndoLastLine;
+  Logger.Success('Load System - Kernel');
+  Sleep(2000);
+
+  Logger.Warn('Load System - DataBase');
+  Sleep(2000);
+  Logger.UndoLastLine;
+  Logger.Error('Load System - DataBase - Erro connection in DataBase');
+  Sleep(2000);
+
+  Logger.Warn('Load System - Drivers');
+  Sleep(2000);
+  Logger.UndoLastLine;
+  Logger.Fatal('Load System - Driver - System Failure');
+
+  Readln;
+end.
