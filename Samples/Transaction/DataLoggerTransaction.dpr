a4b45e409b51ff25d6694feecd414fe8e2f79421
Danilo Lucas <developer.dlio@gmail.com>
2023-04-14 14:14:09 -0300

    Update DataLoggerTransaction.dpr

--- Samples/Transaction/DataLoggerTransaction.dpr
+++ Samples/Transaction/DataLoggerTransaction.dpr
@@ -13,7 +13,7 @@ begin
   Logger.AddProvider(TProviderConsole.Create.UseTransaction(True));
 
   // Definindo o formato do log
-  Logger.SetTemplate('${timestamp} [${type}] ${message}');
+  Logger.SetTemplate('${timestamp} [${level}] ${message}');
 
   // Iniciando a transaction
   Logger.StartTransaction;
